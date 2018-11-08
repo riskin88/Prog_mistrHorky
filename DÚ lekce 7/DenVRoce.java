32081a922da03926814b1e6e00564a57f276f4d6
riskinDesktop <hlavac.honza@email.cz>
2018-11-08 20:54:38 +0100

    Spravil jsem ukol

--- DÚ lekce 7/DenVRoce.java
+++ DÚ lekce 7/DenVRoce.java
@@ -1,40 +1,22 @@
 public class DenVRoce {
     public static void main(String[] args) {
-      int rok = Integer.parseInt(args[0]);
-      int mesic = Integer.parseInt(args[1]);
-      int den = Integer.parseInt(args[2]);
-      int celkemdni = 0;
-      awh.IntList mesice = awh.IntList.create(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-      if (jePrestupny(rok)){
-      awh.Int.List.add(29);
-      }
-      else{
-        awh.Int.List.add(28);
-      }
-      for (int i = 1; i <= mesic; i++){
-        celkemdni += pocetDni (i);           
-    }
+		int rok = Integer.parseInt(args[0]);
+		int mesic = Integer.parseInt(args[1]);
+		int den = Integer.parseInt(args[2]);
+		int celkemdni = 0;
+		int[] mesice = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		if (jePrestupny(rok)){
+		mesice[1]++;
+		}
+		for (int i = 0; i < mesic - 1; i++){
+        celkemdni += mesice[i];           
+		}
       celkemdni += den;
       System.out.printf("%d\n", celkemdni);
-}
-    public static boolean jePrestupny(int rok) {
-      return (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0);
-}
-    public static int pocetDni (int mesic, int rok){
-      if (mesic % 2 == 0){
-        if (!jePrestupny(rok) && mesic == 2){
-          return 28;
-          }
-        else if(jePrestupny(rok) && mesic == 2){
-          return 29;
-          }
-        else {
-        return 30;
-        }
-      }
-      else{
-        return 31;}
-      }
+	}
+	public static boolean jePrestupny(int rok) {
+		return (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0);
+	}
 }
       
         
