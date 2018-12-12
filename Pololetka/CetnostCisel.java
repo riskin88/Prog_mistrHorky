b0669ee7a34e9d1116c6d537ab92d462776524d0
riskinDesktop <hlavac.honza@email.cz>
2018-12-12 21:21:58 +0100

    Pridano serazeni podle cetnosti - Bubble sort

--- Pololetka/CetnostCisel.java
+++ Pololetka/CetnostCisel.java
@@ -25,9 +25,23 @@ public class CetnostCisel {
 				 }
 			 }
 		 }
+		 bubbleSort(cisla);
 		 for (int i = 0; i < cisla.size(); i++){
 			 System.out.printf("%dx %d, ", cisla.get(i)[1], cisla.get(i)[0]);
 		 }
 	}
+	
+	public static void bubbleSort(List<int[]> cisla){
+		for (int i = 0; i < cisla.size() - 1; i++) {
+			for (int j = 0; j < cisla.size() - i - 1; j++) {
+				if (cisla.get(j)[1] < cisla.get(j + 1)[1]){
+					int[] docasny = cisla.get(j);
+					cisla.set(j, cisla.get(j + 1));
+					cisla.set(j + 1, docasny);
+				}
+			}
+		}
+	}  
+
 }
 			
