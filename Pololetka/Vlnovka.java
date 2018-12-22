d2432ef34542c513cbb3413c99e5b9d959e2b73d
riskinDesktop <hlavac.honza@email.cz>
2018-12-22 17:09:08 +0100

    Vlnovka za 1 bod

---
+++ Pololetka/Vlnovka.java
@@ -0,0 +1,27 @@
+public class Vlnovka {
+	
+	public static void main(String[] args) {
+		int vyska = Integer.parseInt(args[0]);
+		int sirka = 80;
+		for (int sloupec = 0; sloupec < vyska; sloupec++){
+			for (int radek = 0; radek < sirka; radek++){
+				if (cozTakhleDatSiKanal(radek, sloupec, vyska)){
+					System.out.printf("#");
+				}
+				else{
+					System.out.printf(" ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+	
+	private static boolean cozTakhleDatSiKanal(int radek, int sloupec, int vyska){
+		if ((radek - sloupec) % (2 * vyska - 2) == 0 || (radek + sloupec) % (2 * vyska - 2) == 0){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
